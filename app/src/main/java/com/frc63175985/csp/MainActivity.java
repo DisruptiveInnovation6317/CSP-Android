6b31814b1f69910d345acb7e85d59c3f239641a0
Brandon Richards <bqrichards@icloud.com>
2019-02-03 18:02:39 -0600

    Create basic scout authentication
    Create a basic authentication process that will later be connected up to the database

--- app/src/main/java/com/frc63175985/csp/MainActivity.java
+++ app/src/main/java/com/frc63175985/csp/MainActivity.java
@@ -11,11 +11,18 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.frc63175985.csp.auth.CSPAuthState;
+import com.frc63175985.csp.auth.CSPAuthStateListener;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, CSPAuthStateListener {
     private static final String LOG_TAG = "CSP-Android";
 
+    private NavigationView navigationView;
+
+    // Fragments
     private WelcomeFragment welcomeFragment;
 
     @Override
@@ -31,12 +38,14 @@ public class MainActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         welcomeFragment = new WelcomeFragment();
         FragmentTransaction fm = getSupportFragmentManager().beginTransaction();
         fm.replace(R.id.content_frame, welcomeFragment).commit();
+
+        CSPAuthState.shared.setStateListener(this);
     }
 
     @Override
@@ -81,5 +90,28 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public static void log(Object o) {
+        Log.d(LOG_TAG, o.toString());
+    }
+
+    @Override
+    public void authStateChanged(boolean loggedIn) {
+        TextView scoutTextView = findViewById(R.id.nav_scout_name);
+        TextView tournamentTextView = findViewById(R.id.nav_tournament_name);
+
+        navigationView.getMenu().getItem(0).setEnabled(loggedIn);
+        navigationView.getMenu().getItem(1).setEnabled(loggedIn);
+
+        if (loggedIn) {
+            // Set title and subtitle to scout and matchlist to user
+            scoutTextView.setText(CSPAuthState.shared.scout);
+            tournamentTextView.setText(CSPAuthState.shared.tournament);
+        } else {
+            // Set title and subtitle to scout and matchlist to placeholders
+            scoutTextView.setText(R.string.nav_header_title);
+            tournamentTextView.setText(R.string.nav_header_subtitle);
+        }
     }
 }
---
+++ app/src/main/java/com/frc63175985/csp/auth/CSPAuthStateListener.java
@@ -0,0 +1,5 @@
+package com.frc63175985.csp.auth;
+
+public interface CSPAuthStateListener {
+    void authStateChanged(boolean loggedIn);
+}
