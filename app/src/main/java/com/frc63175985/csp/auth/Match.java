412930a63a432fce0f7f0c6d0fe7c7290dd10c08
Brandon Richards <bqrichards@icloud.com>
2019-04-10 09:13:33 -0500

    Add field diagram to Alliance and Drive Station

--- app/src/main/java/com/frc63175985/csp/auth/Match.java
+++ app/src/main/java/com/frc63175985/csp/auth/Match.java
@@ -46,7 +46,6 @@ import static com.frc63175985.csp.enums.ScoreObject.HATCH;
  */
 public class Match {
     public static final String[] SPEED_OPTIONS = {"--------", "Slow (>7 sec)", "Med (3-7 sec)", "Fast (<3 sec)"};
-    public static final String[] LEVELS_OPTIONS = {"--------", "1", "2", "3"};
 
     /* Keys */
     public static final String AUTO_PREFIX = "auto_";
@@ -204,7 +203,7 @@ public class Match {
             }
         }
 
-        data.put(key, value);
+        set(key, value);
     }
 
     public void updateCargoShipValue(BaseScoutType type, CargoShipSelection selection, ScoreObject object, Object value) {
@@ -260,7 +259,7 @@ public class Match {
             }
         }
 
-        data.put(key, value);
+        set(key, value);
     }
 
     /**
